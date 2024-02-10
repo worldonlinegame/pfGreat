c9415d859e3cdc994d9c456cb07f57f4ef193f4c
worldonlinegame <motar3000@hotmail.com>
2024-02-10 20:42:07 +0100

    mod bussinesLogic.js

--- js/bussinesLogic.js
+++ js/bussinesLogic.js
@@ -8,12 +8,19 @@ function addFriendToSystem(email,datatime, despcripcion, evento, SelectCell) {
         evento: evento,
         SelectCell: SelectCell
       };
+      if(email == "" || datatime == "" || despcripcion == "" || evento == ""){
+        alert('ERROR ! No se puede guardar si los campos estan vacios, tienes que rellenar todos los campos!');
+        return;
+      }else{
       console.log(newFriend);
       friendList.push(newFriend);
       localStorageFriendList(friendList);
+    }
+      
 }
 
-//funcion devuelve eventos
+
+//funcion devuelve los eventos
 
 function getFriendList() {
     var storedList = localStorage.getItem("localFriendList");
@@ -25,8 +32,12 @@ function getFriendList() {
     }
 
     return friendList;
+    
   }
+
 
 function localStorageFriendList(pList) {
     localStorage.setItem("localFriendList", JSON.stringify(pList));
+    
   }
+  
