04271927bd555acdf45d79984796acb4d0a910a2
worldonlinegame <motar3000@hotmail.com>
2024-02-11 23:45:43 +0100

    mod uiLogic.js

--- js/uiLogic.js
+++ js/uiLogic.js
@@ -1,36 +1,45 @@
 document.querySelector('#btnSaveFriend').addEventListener('click', seveFriend);
- 
-  drawFriendsTable();
+document.querySelector('#btnAssignPet').addEventListener('click', addPet);
 
-  function seveFriend() {
+drawFriendsTable();
+drawPetsTable();
+
+function seveFriend() {
+
     var sEmail = document.querySelector('#email').value,
-     sDatatime = document.querySelector('#datatime').value,
+     sDatatime = document.querySelector('#datatime').value,  //data time
      sDespcripcion = document.querySelector('#despcripcion').value,
      sEvento = document.querySelector('#evento').value;
-  
-    addFriendToSystem(sEmail, sDatatime, sDespcripcion, sEvento);
-    drawFriendsTable();
-  
+
+ var datos = addFriendToSystem(sEmail, sDatatime, sDespcripcion, sEvento);
+  if(!datos){  
+    
+    return true;
+
+  }else{
+      drawFriendsTable(datos)
+      }
   }
 
   //imprimer eventos
   function drawFriendsTable(){
+    
     var list = getFriendList(),
         tbody = document.querySelector('#friendsTable tbody');
     
     tbody.innerHTML = '';
-
+    
     for (var i = 0; i < list.length; i++) {
 
       var row = tbody.insertRow(i),
           EmailCell = row.insertCell(0),
-          DatatimeCell = row.insertCell(1),
+          DatatimeCell = row.insertCell(1),            // este datatime
           DespcripcionCell = row.insertCell(2),
           EventoCell = row.insertCell(3);
           selectCell = row.insertCell(4);
       
       EmailCell.innerHTML = list[i].email;
-      DatatimeCell.innerHTML = list[i].datatime;
+      DatatimeCell.innerHTML = list[i].datatime;       // esta datatime
       DespcripcionCell.innerHTML = list[i].despcripcion;
       EventoCell.innerHTML = list[i].evento;
 
@@ -38,9 +47,54 @@ document.querySelector('#btnSaveFriend').addEventListener('click', seveFriend);
       var inputSelect = document.createElement('input');
       inputSelect.type = 'radio';
       inputSelect.value = list[i].email;
-      inputSelect.name = 'select';
+      inputSelect.name = 'rbtFriend';
       selectCell.appendChild(inputSelect);
 
       tbody.appendChild(row);
     }
+   
+  }
+
+  // funcion de añadir pet "el comentario del post evento"
+
+  function addPet(){
+    var sName = document.querySelector('#txtPetName').value,
+
+        sType = document.querySelector('#txtAnimal').value,  
+
+        sGender = document.querySelector('#txtGender').value,
+        sOwnerId = document.querySelector('#friendsTable tbody input[type=radio]:checked').value;
+        
+    var friendObj = findFriend(sOwnerId);
+   
+
+    addPetToFriend(friendObj, sName,sType,sGender);
+    drawPetsTable();
+
+  }
+
+  // dibujamos la tabla de los post de eventos pets
+
+  function drawPetsTable(){
+    var list = getFriendList(),
+        tbody = document.querySelector('#petsTable tbody');
+    tbody.innerHTML = '';
+    /*Ciclo para recorrer la lista de amigos*/
+    for(var i = 0; i < list.length; i++){
+      /*Ciclo que recorrer la lista de mascotas de cada amigo */
+      for(var j = 0; j < list[i].petList.length; j++){
+        var row = tbody.insertRow(j),
+        ownerCell = row.insertCell(0),
+        nameCell = row.insertCell(1), 
+        typeCell = row.insertCell(2),
+        genderCell = row.insertCell(3);
+
+        ownerCell.innerHTML = list[i].email;
+        nameCell.innerHTML = list[i].petList[j].name;
+        typeCell.innerHTML = list[i].petList[j].type;
+        genderCell.innerHTML = list[i].petList[j].gender;
+
+      }
+    }
+
   }
